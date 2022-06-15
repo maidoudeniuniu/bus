3eadce0eb7af72db213caa8f77e3fb54efe40bce
aniu <767159519@qq.com>
2022-06-15 18:32:08 +0800

    修改站点

--- public/map/qq.js
+++ public/map/qq.js
@@ -1,23 +1,10 @@
-/*
- * @Descripttion: 
- * @version: 
- * @Author: 曾利锋[阿牛]
- * @Date: 2022-06-14 17:50:04
- * @LastEditors: 曾利锋[阿牛]
- * @LastEditTime: 2022-06-14 19:12:46
- */
-/*
- * @Descripttion: 
- * @version: 
- * @Author: 曾利锋[阿牛]
- * @Date: 2022-06-14 17:24:05
- * @LastEditors: 曾利锋[阿牛]
- * @LastEditTime: 2022-06-14 17:48:49
- */
+
 var $cmap;
+var $styles;
 class QQMAP{
   constructor (options) {
     this.$options = options;
+    this.$data = options.data
     this.$callback = options.callback
     this.$callbackList = [] //回调对列表收集
     this.$key= options.key || "84089e5b76b44cffaa5e719c60efc745"; //高德地图key
@@ -29,39 +16,64 @@ class QQMAP{
     this.$siteName = options.siteName || "乐清211路"; //线路
     this.$color = options.color || ['#1BBC60','#ff9900','#ff0000']
     this.$lineSearchData; //线路数据
-    this.$sitePath = options.sitePath ||  []; // 线路数据
-    this.$site = options.site || []; //站点列表
+    this.$sitePath = this.$data.sitePath ||  []; // 线路数据
+    this.$site = this.$data.site || []; //站点列表
     this.$cheSitePath = [] //渲染缓存数据
     this.$clearMapPath = [];
     this.$cheCar = [] //车辆覆盖物缓存
     this.$circleMap = [] // 创建圆形数据
     this.$siteTxt = [] // 站点名称
+    this.$markerList = [];
     this.$siteTxtlock = -1;
     this.$polylineLayer;
+    this.$markerLayer;
+    this.$circleLayer;
+    this.$siteLayer;
     this.$fetchList = []
     this.$cmap;
     this.$clearCmapTime;
     this.$curPage = 2;
     this.$size = 50;
-    this.$totalPage = 1
-    if(options.sitePath){
-      this.$totalLimit = options.sitePath.length
+    this.$totalPage = 1;
+    if(this.$data.sitePath){
+      this.$totalLimit = this.$data.sitePath.length
     } 
-    this.$times = 60000000 
+    this.$times = 60000
     this.$cmap =  $cmap = new TMap.Map(this.$el,{
-      zoom:16	//缩放级别
+      zoom:16,	//缩放级别
+      center: new TMap.LatLng(28.083382,120.85897)
     });  
-    this.polylineLayer();   
+    this.$busicon = this.$data.busicon
+
     if(this.$mode === "auto"){
       this.lineSearch(this.$siteName)
+      this.polylineLayer();
     }else{
       this.init()
     }
+    console.log(this.$data.siteIcon)
   }
   init () {
+    this.styles() /// 样式定义
     this.drawbusLine(this.$sitePath);
-    // this.drawbusCircle();
+    this.drawbusCircle();
+    this.getFetch()
     this.startTime(); 
+   
+  }
+  styles () {
+    $styles = {}
+    this.$color.forEach((item,index)=>{
+      $styles[index+1] =  new TMap.PolylineStyle({
+        'color': item, //线填充色
+        'width': 8, //折线宽度
+        'borderWidth': 1, //边线宽度
+        'borderColor': '#FFF', //边线颜色
+        'lineCap': 'round', //线端头方式
+      })
+    })
+    
+    console.log($styles)
   }
   // 创建覆盖物
   polylineLayer () {
@@ -94,11 +106,18 @@ class QQMAP{
     })
   }
   setSiteCar (data) {
-    this.$cheCar.forEach(item=>this.$cmap.remove(item));
-    data.forEach(item=>{
-      let marker = this.marker(item)
-      this.$cheCar.push(marker)
-      this.$cmap.add(marker)
+    this.$cheCar.forEach(item=> this.$markerLayer.remove(item));
+    this.$cheCar = [];
+    data.forEach((item,index)=>{
+      let id = "bus_"+index;
+      this.marker({
+        styleId:"bus",
+        id:id,
+        lng:item.lng,
+        lat:item.lat
+      }) 
+      this.$cheCar.push(id)
+      // this.marker(marker)
     })
   }
   lineSearch (siteName) {
@@ -148,11 +167,11 @@ class QQMAP{
       this.$totalLimit = this.$sitePath.length
       this.$colorLine = Array(this.$sitePath.length).fill("green");
       this.$cheSitePath = []; // 更新缓存数据
-      // this.getFetch()
+      this.getFetch()
       this.drawbusLine(this.$sitePath)
-      // this.drawbusCircle();
+      this.drawbusCircle();
       //启动定时器 更新线路
-      // this.startTime()  
+      this.startTime()  
     }
   }
   startTime (time = 3000) {
@@ -164,16 +183,17 @@ class QQMAP{
   getFetch () {
     this.$sitePath.forEach((item)=>{
       let key = this.$key;
-      let origins = item[0][0] + "," +item[0][1]; 
-      let destination =item[1][0] + "," + item[1][1];
+      let origins = item[0][1] + "," +item[0][0]; 
+      let destination =item[1][1] + "," + item[1][0];
       // 这里为什么不加途中点，是因为这个驾车模式，和公交线路不一样的，95%一样，还是5%有可能要绕路，
-      // 业务场景例如：比如有些线路公交车可以过，私家车不能过的
-      let str = `https://restapi.amap.com/v3/direction/driving?key=${key}&origin=${origins}&destination=${destination}&extensions=base`
+      // 业务场景例如：比如有些线路公交车可以过，私家车不能过的 
+      let str = `https://apis.map.qq.com/ws/direction/v1/driving/?key=${key}&from=${origins}&to=${destination}&output=jsonp`
       this.$fetchList.push(str) 
     })
   }
   startLine () { 
     if(this.$curPage == this.$totalLimit-1){
+
       this.$clearCmapTime && clearTimeout(this.$clearCmapTime) // 取消定时器
       this.$curPage = 1
       this.$clearCmapTime = setTimeout(()=>{
@@ -182,15 +202,20 @@ class QQMAP{
       return
     }
     let str = this.$fetchList[this.$curPage -1]
-    fetch(str,{
-      method:"get"
-    }).then(res=>res.json()).then((res)=>{
-      this.$cheSitePath[this.$curPage] && this.$cmap.remove(this.$cheSitePath[this.$curPage])
-      let newItem = res.route.paths[0]
-      let color = this.distanceColor(newItem.distance / newItem.duration,newItem.distance)
-      let busPolyline = this.drawbusLineItem(this.$sitePath[this.$curPage],color)
-      // this.$cmap.setMap(busPolyline);
-      this.$cheSitePath[this.$curPage] = busPolyline
+    fetchJsonp(str,{
+      method:"get",
+      jsonpCallbackFunction: 'function_name_of_jsonp_response'
+    }).then(res=>res.json()).then((res)=>{ 
+      let newItem = res.result.routes[0];
+      this.$polylineLayer && this.$polylineLayer.remove('p1_'+this.$curPage)
+      let item = this.$sitePath[this.$curPage]
+      let p1 = {
+        id:'p1_'+this.$curPage,
+        styleId:this.distanceColor(parseInt(newItem.distance / newItem.duration)),
+        paths:[new TMap.LatLng(item[0][1], item[0][0]), new TMap.LatLng(item[1][1], item[1][0])]
+      }   
+      console.log(p1)
+      this.drawbusLineItem(p1)
       this.$curPage++  
       setTimeout(()=>{
         this.startLine()
@@ -198,75 +223,115 @@ class QQMAP{
     }) 
   }
   // 路况情况颜色
-  distanceColor(time,distance) {
-    let flag = this.$color[0];
-    let list = this.$color;
+  distanceColor(time,distance) { 
+    let flag = 1;
     if (time < 3 && distance>1) {
-      flag = list[2]
+      flag = 3
     } else if (time < 4 && distance>1) {
-      flag = list[1]
+      flag = 2
     } else {
-      flag = list[0]
+      flag = 1
     }
     return flag
   } 
   //创建绘画圆形
-  drawbusCircle (){
+  drawbusCircle (){ 
+    this.clearMarker()
     this.$circleMap.forEach(item=>{
-      this.$cmap.remove(item)
+      this.$circleLayer && this.$circleLayer.remove(item)
     })
     this.$siteTxt.forEach(item=>{
-      this.$cmap.remove(item)
-    }) 
-    this.$site.forEach((item,index)=>{
-      this.drawbusCircleItem({lng:item.lng,lat:item.lat,name:item.name,index:index})
+      this.$siteLayer && this.$siteLayer.remove(item)
     })
+    
+    this.$site.forEach((item,index)=>{
+      this.$siteTxt.push('t_'+index) 
+      this.drawbusCircleItem(item,index) 
+      if(this.$siteTxtlock > -1 && this.$siteTxtlock == index){
+        this.marker(Object.assign({},item,{id:'m_0'}))
+      }else{
+        this.drawbusTxt(item,index)
+      } 
+    }) 
   }
-  drawbusCircleItem (item) { 
-    let style = {
-      fontSize:"12px"
-    }
-    let circle = new AMap.Circle({
-      center: new AMap.LngLat(item.lng,item.lat),  // 圆心位置
-      radius: 1, // 圆半径
-      fillColor: '#fff',   // 圆形填充颜色
-      strokeColor: 'green', // 描边颜色
-      strokeWeight: 1, // 描边宽度
-      fillOpacity:1,
-      zIndex:100
-    }); 
-    var text;
-    if(this.$siteTxtlock > -1 && this.$siteTxtlock == item.index){
-      text = new AMap.Marker({
-              position:[item.lng,item.lat],   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
-              // title: '北京',
-              size: new AMap.Size(64, 72),
-              // anchor: 'center', 
-              // imageSize: new AMap.Size(20, 20),
-              zIndex:100,
-              offset: new AMap.Pixel(-32, -64),
-              icon: './site.png', // 添加 Icon 图标 URL
-            });
+  drawbusTxt (item,index) { 
+    if(this.$siteLayer){
+      this.$siteLayer.add({
+        id: 't_'+index, // 点图形数据的标志信息
+        styleId: 'label', // 样式id
+        position: new TMap.LatLng(item.lat, item.lng), // 标注点位置
+        content: item.name, // 标注文本
+        properties: {
+          // 标注点的属性数据
+          title: 'label',
+        },
+      })
     }else{
-      text = new AMap.Text({
-        text:item.name,
-        anchor:'center', // 设置文本标记锚点
-        draggable:false,
-        cursor:'pointer', 
-        style:style,
-        position: [item.lng,item.lat]
+      this.$siteLayer = new TMap.MultiLabel({
+        id: 'label-layer',
+        map:this.$cmap,
+        styles: {
+          label: new TMap.LabelStyle({
+            color: '#333', // 颜色属性
+            size: 12, // 文字大小属性
+            offset: { x: 0, y: 0 }, // 文字偏移属性单位为像素
+            angle: 0, // 文字旋转属性
+            alignment: 'center', // 文字水平对齐属性
+            verticalAlignment: 'middle', // 文字垂直对齐属性
+            backgroundColor:"#fff",
+            padding:'6px 10px',
+            borderWidth:"1px",
+            borderRadius:"5px",
+            borderColor:"#000"
+          }),
+        },
+        geometries: [
+          {
+            id: 't_'+index, // 点图形数据的标志信息
+            styleId: 'label', // 样式id
+            position: new TMap.LatLng(item.lat, item.lng), // 标注点位置
+            content: item.name, // 标注文本
+            properties: {
+              // 标注点的属性数据
+              title: 'label',
+            },
+          },
+        ],
+      });
+      this.$siteLayer.on("click",(e)=>{ 
+        this.$siteTxtlock = e.geometry.id.replace(/t_/g,'');
+        this.drawbusCircle() 
+      })
+    }  
+  }
+  drawbusCircleItem (item,index) {  
+    if(this.$circleLayer){
+      this.$circleLayer.add({
+        id:"c_"+index,
+        styleId: 'circle',
+        center: new TMap.LatLng(item.lat, item.lng),
+        radius: 1,
+      }) 
+    }else{
+      this.$circleLayer = new TMap.MultiCircle({ 
+        map:this.$cmap,
+        styles: { // 设置圆形样式
+          'circle': new TMap.CircleStyle({
+            'color': 'rgba(255,255,255,1)',
+            'showBorder': true,
+            'borderColor': 'rgba(41,91,255,1)',
+            'borderWidth': 1
+          }),
+        },
+        geometries: [{
+          id:"c_"+index,
+          styleId: 'circle',
+          center: new TMap.LatLng(item.lat, item.lng),
+          radius: 1,
+        }],
       });
     }
-    
-    text.on('click',(e)=>{ 
-      this.$siteTxtlock = item.index;
-      this.drawbusCircle() 
-      this.forCallback(item)
-    })
-    this.$circleMap.push(circle)
-    this.$siteTxt.push(text)
-    circle.setMap(this.$cmap);
-    text.setMap(this.$cmap); 
+    this.$circleMap.push("c_"+index)  
   }
   callback (fn) {
     this.$callbackList.push(fn)
@@ -276,38 +341,64 @@ class QQMAP{
     this.$callbackList.forEach(item=>item(data))
   }
   // 绘画线路Polyline
-  drawbusLine () {
-    // let busPolyline =  this.drawbusLineItem(this.$sitePath[0])
-    this.$polylineLayer.add({
-      'id': 0,  
-      'styleId': 'style_red', 
-      paths:[this.$sitePath[0]]
-    })
-    
-    // this.$sitePath.forEach((item,index)=>{
-    //   this.$cheSitePath[index] && this.$cmap.remove(this.$cheSitePath[index])
-    //   let busPolyline =  this.drawbusLineItem(item)
-    //   // this.$cmap.setMap(busPolyline);
-    //   // this.$cmap.setFitView();
-    //   this.$cheSitePath.push(busPolyline)
-    // })
+  drawbusLine () { 
+    this.$sitePath.forEach((item,index)=>{
+      let str = parseInt(parseInt(Math.random()*10)/4);  
+      let p1 = {
+        id:'p1_'+index,
+        styleId:1,
+        paths:[new TMap.LatLng(item[0][1], item[0][0]), new TMap.LatLng(item[1][1], item[1][0])]
+      }   
+      this.$polylineLayer && this.$polylineLayer.remove('p1_'+index) // 绘画之前，先删除之前的数据
+      this.drawbusLineItem([p1])
+    }) 
   }
-  drawbusLineItem (arrPath,color=this.$color[0]) { 
-    
-    // console.log("polyLine",polyLine)
-    // return polyLine 
-  }
-  marker (item) {
-    return new AMap.Marker({
-          position: new AMap.LngLat(item.lng, item.lat),   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
-          // title: '北京',
-          size: new AMap.Size(32, 36),
-          anchor: 'center', 
-          imageSize: new AMap.Size(20, 20),
-          zIndex:20,
-          // offset: new AMap.Pixel(0, -10),
-          icon: './busicon.png', // 添加 Icon 图标 URL
+  drawbusLineItem (arrPath,color=this.$color[0]) {
+    if(this.$polylineLayer){  
+      this.$polylineLayer.add(arrPath)
+    }else{
+       //创建 MultiPolyline显示折线
+      this.$polylineLayer = new TMap.MultiPolyline({
+        id: 'polyline-layer', //图层唯一标识
+        map: $cmap,//绘制到目标地图
+        //折线样式定义
+        styles: $styles,
+        //折线数据定义
+        geometries: arrPath
       });
+    }   
+  }
+  //创建覆盖物
+  marker (item) { 
+    if(!this.$markerLayer){
+      this.$markerLayer =new TMap.MultiMarker({
+          id: 'marker-layer',
+          map: this.$cmap,
+          styles: {
+            "site": new TMap.MarkerStyle({
+              "width": 64,
+              "height": 72,
+              "anchor": { x: 32, y: 64 },
+              "src": this.$data.siteIcon
+            }),
+            "bus": new TMap.MarkerStyle({
+              "width": 32,
+              "height": 36,
+              "anchor": { x: 32, y: 64 },
+              "src": this.$data.busicon
+            })
+          },
+      });
+    }
+    this.$markerLayer.add({
+      id:item.id,
+      styleId: item.styleId || 'site',
+      position:new TMap.LatLng(item.lat, item.lng)
+    }) 
+  }
+  clearMarker () {
+    this.$markerLayer && this.$markerLayer.setMap(null)
+    this.$markerLayer = null
   }
 } 
 window.Cmap = QQMAP
---
+++ public/map/qqt.js
@@ -0,0 +1,342 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 曾利锋[阿牛]
+ * @Date: 2022-06-14 17:50:04
+ * @LastEditors: 曾利锋[阿牛]
+ * @LastEditTime: 2022-06-15 11:26:51
+ */
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 曾利锋[阿牛]
+ * @Date: 2022-06-14 17:24:05
+ * @LastEditors: 曾利锋[阿牛]
+ * @LastEditTime: 2022-06-14 17:48:49
+ */
+var $cmap;
+class QQMAP{
+  constructor (options) {
+    this.$options = options;
+    this.$callback = options.callback
+    this.$callbackList = [] //回调对列表收集
+    this.$key= options.key || "84089e5b76b44cffaa5e719c60efc745"; //高德地图key
+    this.$mode = options.mode || "auto"
+    this.$type = options.type || "GD"; //模式 GD 高德  baidu 百度  tc 腾讯
+    this.$el = options.el || "map";  //地图domid
+    this.$initParam  = options.initParam || {} //初始化参数
+    this.$LineSearch = options.LineSearch || {} 
+    this.$siteName = options.siteName || "乐清211路"; //线路
+    this.$color = options.color || ['#1BBC60','#ff9900','#ff0000']
+    this.$lineSearchData; //线路数据
+    this.$sitePath = options.sitePath ||  []; // 线路数据
+    this.$site = options.site || []; //站点列表
+    this.$cheSitePath = [] //渲染缓存数据
+    this.$clearMapPath = [];
+    this.$cheCar = [] //车辆覆盖物缓存
+    this.$circleMap = [] // 创建圆形数据
+    this.$siteTxt = [] // 站点名称
+    this.$siteTxtlock = -1;
+    this.$polylineLayer;
+    this.$fetchList = []
+    this.$cmap;
+    this.$clearCmapTime;
+    this.$curPage = 2;
+    this.$size = 50;
+    this.$totalPage = 1
+    if(options.sitePath){
+      this.$totalLimit = options.sitePath.length
+    } 
+    this.$times = 60000000 
+    this.$cmap =  $cmap = new TMap.Map(this.$el,{
+      zoom:16,	//缩放级别
+      center: new TMap.LatLng(28.083382,120.85897)
+    });  
+    if(this.$mode === "auto"){
+      this.lineSearch(this.$siteName)
+      this.polylineLayer();
+    }else{
+      this.init()
+    }
+  }
+  init () {
+    this.drawbusLine(this.$sitePath);
+    // this.drawbusCircle();
+    this.startTime(); 
+  }
+  // 创建覆盖物
+  polylineLayer () {
+    this.$polylineLayer = new TMap.MultiPolyline({
+      map: $cmap, // 绘制到目标地图
+      // 折线样式定义
+      styles: {
+        'style_blue': new TMap.PolylineStyle({
+          color: '#3777FF', //线填充色
+          width: 10, //折线宽度
+          borderWidth: 0, //边线宽度
+          showArrow: true,
+          arrowOptions: {
+            space: 70
+          },
+          lineCap: 'round',
+        }),
+      }
+    });
+  } 
+  //添加数据变化
+  setSitePath (data) { 
+    this.$clearCmapTime && clearInterval(this.$clearCmapTime) // 取消定时器 
+    data.forEach((item,index)=>{
+      this.$cheSitePath[index] && this.$cmap.remove(this.$cheSitePath[index]) 
+      let color = this.distanceColor(item.distance / item.duration,item.distance) 
+      let busPolyline = this.drawbusLineItem(this.$sitePath[index],color)
+      this.$cmap.add([busPolyline]);
+      this.$cheSitePath[index] = busPolyline
+    })
+  }
+  setSiteCar (data) {
+    this.$cheCar.forEach(item=>this.$cmap.remove(item));
+    data.forEach(item=>{
+      let marker = this.marker(item)
+      this.$cheCar.push(marker)
+      this.$cmap.add(marker)
+    })
+  }
+  lineSearch (siteName) {
+    this.$clearCmapTime && clearInterval(this.$clearCmapTime) // 取消定时器
+    this.$cmap.destroy()  // 创建之前，现在清除之前覆盖物
+    let _this = this; 
+    if(!siteName) return;
+    this.$lineSearchData = new AMap.LineSearch(Object.assign(
+      {
+        pageIndex: 1,
+        city: '乐清市',
+        pageSize: 1,
+        extensions: 'all'
+      },this.$LineSearch
+    )) 
+    this.$lineSearchData.search(siteName, function(status, result) {
+       if (status === 'complete' && result.info === 'OK') {
+        _this.lineSearch_Callback(result); 
+       } else {
+        alert(result);
+       }
+   });
+  } 
+  lineSearch_Callback (data) { 
+    // console.log(data)
+    var lineArr = data.lineInfo;
+    var lineNum = data.lineInfo.length;
+    if (lineNum == 0) {
+    } else {
+      this.$site = lineArr[0].via_stops.map(item=>{
+        return {
+          id:item.id,
+          name:item.name,
+          lng:item.location.lng,
+          lat:item.location.lat
+        }
+      }); 
+      this.$sitePath = lineArr[0].path.reduce((cur,per,index,arr)=>{
+        if(arr.length-1 == index){
+          return cur
+        }
+        cur.push([[per.lat,per.lng],[arr[index+1].lng,arr[index+1].lat]])
+        return cur
+      },[]); 
+      // console.log(JSON.stringify(this.$sitePath))
+      this.$totalPage = Math.ceil(this.$sitePath.length / this.$size)
+      this.$totalLimit = this.$sitePath.length
+      this.$colorLine = Array(this.$sitePath.length).fill("green");
+      this.$cheSitePath = []; // 更新缓存数据
+      // this.getFetch()
+      this.drawbusLine(this.$sitePath)
+      // this.drawbusCircle();
+      //启动定时器 更新线路
+      // this.startTime()  
+    }
+  }
+  startTime (time = 3000) {
+    this.$clearCmapTime = setTimeout(()=>{
+      this.startLine()
+    },time)  
+  }
+  // 缓存各个fetch记录
+  getFetch () {
+    this.$sitePath.forEach((item)=>{
+      let key = this.$key;
+      let origins = item[0][0] + "," +item[0][1]; 
+      let destination =item[1][0] + "," + item[1][1];
+      // 这里为什么不加途中点，是因为这个驾车模式，和公交线路不一样的，95%一样，还是5%有可能要绕路，
+      // 业务场景例如：比如有些线路公交车可以过，私家车不能过的
+      let str = `https://restapi.amap.com/v3/direction/driving?key=${key}&origin=${origins}&destination=${destination}&extensions=base`
+      this.$fetchList.push(str) 
+    })
+  }
+  startLine () { 
+    if(this.$curPage == this.$totalLimit-1){
+      this.$clearCmapTime && clearTimeout(this.$clearCmapTime) // 取消定时器
+      this.$curPage = 1
+      this.$clearCmapTime = setTimeout(()=>{
+        this.startLine()
+      },this.$times)
+      return
+    }
+    let str = this.$fetchList[this.$curPage -1]
+    fetch(str,{
+      method:"get"
+    }).then(res=>res.json()).then((res)=>{
+      this.$cheSitePath[this.$curPage] && this.$cmap.remove(this.$cheSitePath[this.$curPage])
+      let newItem = res.route.paths[0]
+      let color = this.distanceColor(newItem.distance / newItem.duration,newItem.distance)
+      let busPolyline = this.drawbusLineItem(this.$sitePath[this.$curPage],color)
+      // this.$cmap.setMap(busPolyline);
+      this.$cheSitePath[this.$curPage] = busPolyline
+      this.$curPage++  
+      setTimeout(()=>{
+        this.startLine()
+      },200)
+    }) 
+  }
+  // 路况情况颜色
+  distanceColor(time,distance) {
+    let flag = this.$color[0];
+    let list = this.$color;
+    if (time < 3 && distance>1) {
+      flag = list[2]
+    } else if (time < 4 && distance>1) {
+      flag = list[1]
+    } else {
+      flag = list[0]
+    }
+    return flag
+  } 
+  //创建绘画圆形
+  drawbusCircle (){
+    this.$circleMap.forEach(item=>{
+      this.$cmap.remove(item)
+    })
+    this.$siteTxt.forEach(item=>{
+      this.$cmap.remove(item)
+    }) 
+    this.$site.forEach((item,index)=>{
+      this.drawbusCircleItem({lng:item.lng,lat:item.lat,name:item.name,index:index})
+    })
+  }
+  drawbusCircleItem (item) { 
+    let style = {
+      fontSize:"12px"
+    }
+    let circle = new AMap.Circle({
+      center: new AMap.LngLat(item.lng,item.lat),  // 圆心位置
+      radius: 1, // 圆半径
+      fillColor: '#fff',   // 圆形填充颜色
+      strokeColor: 'green', // 描边颜色
+      strokeWeight: 1, // 描边宽度
+      fillOpacity:1,
+      zIndex:100
+    }); 
+    var text;
+    if(this.$siteTxtlock > -1 && this.$siteTxtlock == item.index){
+      text = new AMap.Marker({
+              position:[item.lng,item.lat],   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+              // title: '北京',
+              size: new AMap.Size(64, 72),
+              // anchor: 'center', 
+              // imageSize: new AMap.Size(20, 20),
+              zIndex:100,
+              offset: new AMap.Pixel(-32, -64),
+              icon: './site.png', // 添加 Icon 图标 URL
+            });
+    }else{
+      text = new AMap.Text({
+        text:item.name,
+        anchor:'center', // 设置文本标记锚点
+        draggable:false,
+        cursor:'pointer', 
+        style:style,
+        position: [item.lng,item.lat]
+      });
+    }
+    
+    text.on('click',(e)=>{ 
+      this.$siteTxtlock = item.index;
+      this.drawbusCircle() 
+      this.forCallback(item)
+    })
+    this.$circleMap.push(circle)
+    this.$siteTxt.push(text)
+    circle.setMap(this.$cmap);
+    text.setMap(this.$cmap); 
+  }
+  callback (fn) {
+    this.$callbackList.push(fn)
+  }
+  forCallback(data) {
+    this.$callback && this.$callback(data)
+    this.$callbackList.forEach(item=>item(data))
+  }
+  // 绘画线路Polyline
+  drawbusLine () {
+    // let busPolyline =  this.drawbusLineItem(this.$sitePath[0])
+    // this.$polylineLayer.add({
+    //   'id': 0,  
+    //   'styleId': 'style_red', 
+    //   paths:[this.$sitePath[0]]
+    // })
+    
+    // this.$sitePath.forEach((item,index)=>{
+    //   this.$cheSitePath[index] && this.$cmap.remove(this.$cheSitePath[index])
+    //   let busPolyline =  this.drawbusLineItem(item)
+    //   // this.$cmap.setMap(busPolyline);
+    //   // this.$cmap.setFitView();
+    //   this.$cheSitePath.push(busPolyline)
+    // })
+    let p1 = this.$sitePath.map(item=>{
+      return new TMap.LatLng(item[0][1], item[0][0])
+    })
+
+   this.drawbusLineItem(p1)
+
+
+  }
+  drawbusLineItem (arrPath,color=this.$color[0]) {  
+    //创建 MultiPolyline显示折线
+    this.$polylineLayer = new TMap.MultiPolyline({
+      id: 'polyline-layer', //图层唯一标识
+      map: $cmap,//绘制到目标地图
+      //折线样式定义
+      styles: {
+        'style_blue': new TMap.PolylineStyle({
+          'color': color, //线填充色
+          'width': 8, //折线宽度
+          'borderWidth': 1, //边线宽度
+          'borderColor': '#FFF', //边线颜色
+          'lineCap': 'round', //线端头方式
+        })
+      },
+      //折线数据定义
+      geometries: [
+        {
+          'id': 'pl_1',//折线唯一标识，删除时使用
+          'styleId': 'style_blue',//绑定样式名
+          'paths': arrPath
+        }
+      ]
+    });
+  }
+  marker (item) {
+    return new AMap.Marker({
+          position: new AMap.LngLat(item.lng, item.lat),   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+          // title: '北京',
+          size: new AMap.Size(32, 36),
+          anchor: 'center', 
+          imageSize: new AMap.Size(20, 20),
+          zIndex:20,
+          // offset: new AMap.Pixel(0, -10),
+          icon: './busicon.png', // 添加 Icon 图标 URL
+      });
+  }
+} 
+window.Cmap = QQMAP
+ 
